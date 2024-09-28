e443cee0249664a84be81fba3eaf82882ceb6012
João Vitor <jvdrdo@gmail.com>
2024-09-28 11:47:54 -0300

    Update trocoMercado.java

--- src/trocoMercado.java
+++ src/trocoMercado.java
@@ -1,60 +1,63 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 
-public class trocoMercado {
+public class Main {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
 
-        List<Double> valoresReal = new ArrayList<>(List.of(100.00, 50.00, 20.00, 10.00,
-                5.00, 2.00, 1.00, 0.5, 0.25, 0.10, 0.05, 0.01));
+        List<BigDecimal> valoresReal = List.of(
+                new BigDecimal("100.00"), new BigDecimal("50.00"), new BigDecimal("20.00"),
+                new BigDecimal("10.00"), new BigDecimal("5.00"), new BigDecimal("2.00"),
+                new BigDecimal("1.00"), new BigDecimal("0.50"), new BigDecimal("0.25"),
+                new BigDecimal("0.10"), new BigDecimal("0.05"), new BigDecimal("0.01"));
 
         System.out.print("Digite o valor da compra: ");
-        String valor = teclado.next();
-        double valorCompra = Double.parseDouble(valor.replace(",", "."));
+        BigDecimal valorCompra = teclado.nextBigDecimal();
 
         System.out.print("Valor recebido: ");
-        valor = teclado.next();
-        double valorRecebido = Double.parseDouble(valor.replace(",", "."));
+        BigDecimal valorRecebido = teclado.nextBigDecimal();
 
-        double diferenca = valorRecebido - valorCompra;
+        BigDecimal diferenca = valorRecebido.subtract(valorCompra);
 
-        if(valorCompra == valorRecebido){
+        if(valorCompra.equals(valorRecebido)){
             System.out.println("Valor correto");
 
-        }else if(valorCompra < valorRecebido){
-            List<Double> trocos = calcularTroco(diferenca, valoresReal);
+        }else if(valorCompra.compareTo(valorRecebido) < 0){
+            List<BigDecimal> trocos = calcularTroco(diferenca, valoresReal);
             System.out.println("------------------------------");
-            System.out.printf("Troco: R$ %.2f%n", diferenca);
+            System.out.println("TROCO R$"+diferenca);
             printarTroco(trocos, valoresReal);
 
         }else{
             System.out.println("Compra negada");
 
         }
-
     }
-    public static List<Double> calcularTroco(double diferenca, List<Double> list){
-        int x = 0;
-        List<Double> trocos = new ArrayList<>();
-        while (diferenca > 0.01){
-            if(diferenca - list.get(x) >= 0){
-                diferenca -=  list.get(x);
-                trocos.add(list.get(x));
 
-            }else{
+    public static List<BigDecimal> calcularTroco(BigDecimal diferenca, List<BigDecimal> list) {
+        int x = 0;
+
+        List<BigDecimal> trocos = new ArrayList<>();
+
+        while (list.size() != x) {
+            if (diferenca.subtract(list.get(x)).compareTo(BigDecimal.ZERO) >= 0) {
+                diferenca = diferenca.subtract(list.get(x));
+                trocos.add(list.get(x));
+            } else {
                 x++;
             }
         }
         return trocos;
     }
 
-    public static void printarTroco(List<Double> troco, List<Double> valoresReal){
-
-        for (Double v : valoresReal) {
-            if (troco.contains(v)){
+    public static void printarTroco(List<BigDecimal> troco, List<BigDecimal> valoresReal) {
+        for (BigDecimal v : valoresReal) {
+            if (troco.contains(v)) {
                 int i = Collections.frequency(troco, v);
-                String tipo = (v > 1)? "Nota":"Moeda";
-                System.out.println(i+" "+tipo+"(s) de R$"+v);
+                String tipo = (v.compareTo(BigDecimal.ONE) > 0) ? "Nota" : "Moeda";
+                System.out.println(i + " " + tipo + "(s) de R$" + v.setScale(2, RoundingMode.HALF_UP));
             }
         }
     }
